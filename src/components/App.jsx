4c3029e9081169fdc42da3361258af562694b944
Caleb Duke <dukecj18@gmail.com>
2020-02-27 15:54:48 -0600

    hey

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import Films from "./Films";
+// import People from "./People";
+
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/films">Films</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/films" component={Films} />
+          <Route exact path="/people/:id" component={SinglePerson} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
